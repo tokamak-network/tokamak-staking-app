64e276209b2cb685ae10101ef9e6a307e1a55d48
Lakmi <lakmi94@gmail.com>
2020-12-24 17:18:21 +0900

    powerTON screen implemented

---
+++ src/components/WinnerTable.vue
@@ -0,0 +1,303 @@
+<template>
+  <view class="winner-table-container">
+    <view class="table-header-container">
+      <view class="th-name" :style="{ width: '15%' }">
+        <text class="th-text">Round</text></view
+      >
+      <view class="th-name" :style="{ width: '30%' }">
+        <text class="th-text">Winner</text></view
+      >
+      <view class="th-name" :style="{ width: '25%' }">
+        <text class="th-text">Reward</text>
+        <text class="th-text">(TON)</text></view
+      >
+      <view class="th-name" :style="{ width: '30%' }">
+        <text class="th-text">End date</text></view
+      >
+    </view>
+    <view class="table-content-container">
+      <scroll-view>
+        <view class="list-row" v-for="winner in winners" :key="winner.round">
+          <view class="list-item" :style="{ width: '15%' }">
+            <text>{{
+              winner.round
+            }}</text>
+             
+          </view>
+          <view  class="list-item" :style="{ width: '30%' }">
+            <text>{{
+              winner.winner
+            }}</text>
+          </view>
+          <view class="list-item" :style="{ width: '25%' }">
+            <text>{{
+              winner.reward
+            }}</text>
+          </view>
+          <view class="list-item" :style="{ width: '30%' }">
+            <text>{{
+              winner.date
+            }}</text>
+          </view>
+         
+        </view>
+      </scroll-view>
+    </view>
+  </view>
+</template>
+<script>
+import React from "react";
+import { View, Text } from "react-native";
+export default {
+  data() {
+    return {
+      winners: [
+        {
+          round: 29,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 28,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 27,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 26,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 25,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 24,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 23,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 22,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 21,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 20,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 19,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 18,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 17,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 16,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 15,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 14,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 13,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 12,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 11,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 10,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 9,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 8,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 7,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 6,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 5,
+          winner: "0xe300...1327",
+          reward: "30,868.54",
+          date: "18/12/2020",
+        },
+        {
+          round: 4,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 3,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 2,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 1,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+        {
+          round: 0,
+          winner: "0x36f9...3E44",
+          reward: "32,580.62",
+          date: "18/12/2020",
+        },
+      ],
+    };
+  },
+  methods: {
+    getFlatListRenderItem(item) {
+      return <Text>{item}</Text>;
+    },
+  },
+};
+</script>
+<style scoped>
+.winner-table-container {
+  width: 90%;
+  background-color: #e2e8eb;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 20px;
+  height: 520;
+  border-width: 1;
+  border-color: #ccd1d3;
+  border-radius: 13;
+  align-self: stretch;
+  position: relative;
+  display: flex;
+  align-items: center;
+  /* padding: 10px; */
+  padding-top: 10px;
+}
+.table-header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+  padding-left: 8px;
+  margin-bottom: 15px;
+}
+.th-name {
+  display: flex;
+  /* justify-content: center; */
+  align-self: stretch;
+  align-items: center;
+}
+.th-text {
+  font-size: 18px;
+  font-weight: bold;
+  color: #555555;
+}
+.list-row {
+  display: flex;
+  flex-direction: row;
+}
+.table-content-container {
+  display: flex;
+  flex-direction: row;
+  height: 84%;
+}
+.list-row {
+  display: flex;
+ 
+  flex-direction: row;
+  
+}
+.list-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 15px;
+}
+</style>
--- src/screens/PowertonScreen.vue
+++ src/screens/PowertonScreen.vue
@@ -2,15 +2,20 @@
     <view class="power-layout">
    <header :navigation="navigation" :back="false"/>
            <view class="power-container">
-         <text>power TON</text>
+         <text class="page-title">Win Power by staking TON!!</text>
+      <text class="page-text"
+        >Increase your chance of winning Power by staking more TON.</text>
+        <winner-table/>
       </view>
     </view>
 </template>
 <script>
-import Header from '@/components/Header'
+import Header from '@/components/Header';
+import WinnerTable from '@/components/WinnerTable';
 export default {
     components: {
       'header': Header,
+      'winner-table': WinnerTable
     },
     props: {
     navigation: {
@@ -31,6 +36,18 @@ export default {
   position: relative;
   display: flex;
   align-items: center;
-  padding-top: 40;
+  padding-top: 20px;
+}
+.page-title {
+  font-size: 30px;
+  text-align: center;
+  color: #555555;
+  padding: 0px 40px;
+  font-weight: 700;
+}
+.page-text {
+  font-size: 20px;
+  text-align: center;
+  padding: 20px 40px;
 }
 </style>

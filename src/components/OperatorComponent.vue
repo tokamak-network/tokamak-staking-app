f9a22478ed3ca7011d2bb215eadf03859815fc4f
Lakmi <lakmi94@gmail.com>
2021-02-23 11:57:12 +0900

    updated

--- src/components/OperatorComponent.vue
+++ src/components/OperatorComponent.vue
@@ -19,13 +19,13 @@
         <view>
           <image
             class="operator-img"
-            :source="operator.avatar === '' ? tokamak : dsrv"
+            :source="tokamak"
             :style="{
               height: windowHeight * 0.122 * 0.45,
               width: componentWidth * 0.11,
               marginRight: componentWidth * 0.056,
               resizeMode: 'contain',
-              opacity: operator.avatar === '' ? 0.12 : 1,
+              opacity: 0.12,
             }"
           >
           </image>
